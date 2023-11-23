78c9f0ec78c9662610b426e617513bcf530993cb
hitheshvar <10hithesh12@gmail.com>
2023-11-24 00:20:37 +0530

    file created

---
+++ games/snakegame.py
@@ -0,0 +1,23 @@
+from tkinter import *
+import random
+
+
+
+class snake:
+    pass
+
+class food:
+    pass
+
+def next_turn():
+    pass
+
+def change_direction(new_direction):
+    pass
+
+def check_collision():
+    pass
+
+def game_over():
+    pass
+
