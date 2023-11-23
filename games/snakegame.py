75e3798c9841d8345add7b5051f3b137db4e3e12
hitheshvar <10hithesh12@gmail.com>
2023-11-24 01:26:10 +0530

    snake game created

--- games/snakegame.py
+++ games/snakegame.py
@@ -1,13 +1,39 @@
 from tkinter import *
 import random
 
+GAME_WIDTH = 700
+GAME_HEIGHT = 700
+SPEED = 50
+SPACE_SIZE = 50
+BODY_PARTS = 3
+SNAKE_COLOR = "#00FF00"
+FOOD_COLOR = "#FF0000"
+BACKGROUND_COLOR ="#000000"
 
+class Snake:
+    
+    def __init__(self):
+        self.body_size = BODY_PARTS 
+        self.coordinates = []
+        self.squares = []
+        
+        for i in range(0, BODY_PARTS):
+            self.coordinates.append([0, 0])
+        
+        for x, y in self.coordinates:
+            square = canvas.create_rectangle(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=SNAKE_COLOR, tag="snake")
+            self.squares.append(square)
 
-class snake:
-    pass
-
-class food:
-    pass
+class Food:
+    
+    def __init__(self):
+        
+        x = random.randint(0, (GAME_WIDTH/SPACE_SIZE)-1) * SPACE_SIZE
+        y = random.randint(0, (GAME_HEIGHT/SPACE_SIZE)-1) * SPACE_SIZE
+        
+        self.coordinates = [x, y]
+        
+        canvas.create_rectangle(x, y, x + SPACE_SIZE, y + SPACE_SIZE, fill=FOOD_COLOR, tag="food")
 
 def next_turn():
     pass
@@ -21,3 +47,34 @@ def check_collision():
 def game_over():
     pass
 
+window = Tk()
+window.title("Snake Game")
+window.resizable(False, False)
+
+score = 0
+direction = 'down'
+
+label = Label(window, text="Score:{}".format(score), font=('consolas', 40))
+label.pack()
+
+canvas = Canvas(window, bg=BACKGROUND_COLOR, height=GAME_HEIGHT, width=GAME_WIDTH)
+canvas.pack()
+
+window.update()
+
+window_width = window.winfo_width()
+window_height = window.winfo_height()
+screen_width = window.winfo_screenwidth()
+screen_height = window.winfo_screenheight()
+
+
+x = int((screen_width/2) - (window_width/2))
+y = int((screen_height/2)- (window_height/2))
+
+window.geometry(f"{window_width}x{window_height}+{x}+{y}")
+
+snake = Snake()
+food = Food()
+
+
+window.mainloop()
